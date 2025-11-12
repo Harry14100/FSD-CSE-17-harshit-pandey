97f0a4cb4eb570970f913cb87f2ab8f08148ca12
Harry14100 <pharshit845@gmail.com>
2025-11-12 16:10:52 +0530

    addition

---
+++ HTML/javascript/popup.js
@@ -0,0 +1,14 @@
+<HTML>
+    <Head>
+        <title> Addition programm using print</title>
+    </Head>
+    <Body>
+        <Script>
+            let res;
+            let first = print("Enter first number: ");
+            let second = print("Enter second number: ");
+            res = parseInt(first) + parseInt(second);
+            document.write("The sum is: " + res);
+        </Script>
+    </Body>
+</HTML>
